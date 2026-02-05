510a10dc329e67b0b8b33abdf7762cddf8d85269
ngtaiphat1501 <taiciute105@gmail.com>
2026-02-05 12:33:57 +0700

    pro192_project_group5

--- src/main/java/com/qlsv/Main.java
+++ src/main/java/com/qlsv/Main.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package main.java.com.qlsv;
 
-/**
- *
- * @author ACER
- */
+package main.java.com.qlsv;
+//import com.qlsv.controller.*;
+//import com.qlsv.service.*;
+//import java.util.Scanner;
+
 public class Main {
     
 }
